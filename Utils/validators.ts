a767d4ef7b1acb4a4d812e48361f5fc326a65d9a
Albert Lee <allilee@ucsc.edu>
2022-08-18 13:18:08 -0700

    linted code

--- Utils/validators.ts
+++ Utils/validators.ts
@@ -32,26 +32,25 @@ class generalValidation {
     for (const key in given_keywords) {
       const given_key = given_keywords[key];
       const key_attributes = this.known_keywords[key];
+      
+      errors[key] = {};
+
       if (given_key.trim() === "") {
-        errors[key] = `${key} must not be empty`;
+        errors[key].lengthError = `${key} must not be empty`;
       }
       
       if (key_attributes.regex && !given_key.match(key_attributes.regex)) {
-        errors[key] = `${key} formatting is invalid`;
+        errors[key].formattingError = `${key} formatting is invalid`;
       }
       
       if (given_key.length < key_attributes.minLength) {
-        errors[key] = `${key} is shorter than ${key_attributes.minLength} characters`;
+        errors[key].lengthError = `${key} is shorter than ${key_attributes.minLength} characters`;
+      }
+      else if (given_key.length > key_attributes.maxLength) {
+        errors[key].lengthError = `${key} is greater than ${key_attributes.maxLength} characters`;
       }
 
-      if (given_key.length > key_attributes.maxLength) {
-        errors[key] = `${key} is greater than ${key_attributes.maxLength} characters`;
-      }
-
-      if (!errors[key]) {
-        errors[key] = "";
-      }
-      else {
+      if (errors[key] !== {}) {
         valid = false;
       }
     }
